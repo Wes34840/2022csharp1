59a7f6b97950f4bb80a775e9a44f3c9d43c00953
Wessel <34840@ma-web.nl>
2023-02-17 12:47:40 +0100

    Variables

---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class Program
+    {
+        //class variables & gebruik
+        static void Main(string[] args)
+        {
+
+            //ga naar de file TwitchStream.cs in je solution explorer in visual studio
+            TwitchStream twitchStream = new TwitchStream();
+
+            //geef nu de stream de volgende waardes:
+            //name, waarde: je favoriete streamer
+            //lastStreamName, waarde: de streamname van de laatste keer
+            //streamGame, waarde een nieuwe instance van Game (new Game("naam van de game hier"})
+            //followers, waarde: hoeveel followers
+            //automod, waarde: staat automod aan?
+            twitchStream.name = "Draqu_";
+            twitchStream.lastStreamName = "Doom Eternal 100% Ultra Nightmare Speedrun";
+            twitchStream.streamGame = new Game("Doom Eternal");
+            twitchStream.followers = 32600;
+            twitchStream.automod = true;
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
