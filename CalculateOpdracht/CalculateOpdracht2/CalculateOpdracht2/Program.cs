892b55d6674e3e8cbaafaac0f02e8b8e6c08ac64
Wessel <34840@ma-web.nl>
2023-02-17 13:43:24 +0100

    calculate opdrachten

---
+++ CalculateOpdracht/CalculateOpdracht2/CalculateOpdracht2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace CalculateOpdracht2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //we gaan nu dingen kopen
+
+            int controllersInMandje = 0;
+
+            controllersInMandje += 2;
+            //koop 2 controllers
+
+
+            //hij is in voorraad snel kopen!
+            int ps5 = 0;
+
+            //koop 1 ps5
+
+            ps5++;
+
+            //koop de games:
+
+            int eldenRing = 0;
+
+            int shantae = 0;
+
+            int spongebob = 0;
+
+            eldenRing++;
+
+            shantae++;
+
+            spongebob++;
+
+            //hmmm weten we het zeker?
+            //haal 1 game weg met -- of -= 1
+
+            shantae--;
+
+            //nu nog de prijs
+
+
+            double ps5Prijs = 499.99;
+            double controllerPrijs = 69.99;
+            double gamePrijs = 59.99;
+
+
+            double totaalControllerPrijs = 0;// controllersInMandje keer controllerPrijs
+
+            totaalControllerPrijs = controllerPrijs * 2;
+
+            double totaalGamePrijs = 0;// game1 keer gamePrijs  + game2 keer gamePrijs (let op je haakjes!)
+
+            totaalGamePrijs = gamePrijs * 2;
+
+            double totaalPrijs = 0;// tel hier alles bij elkaar op
+
+            totaalPrijs = ps5Prijs + totaalControllerPrijs + totaalGamePrijs;
+
+            //er moet nu 759.95 staan
+            Console.Write(totaalPrijs);
+        }
+    }
+}
---
+++ CalculateOpdracht/CalculateOpdracht3/CalculateOpdracht3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CalculateOpdracht2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            double totaalPrijs = 759.95;// tel hier alles bij elkaar op
+
+            //oof wat duur, maar we hebben een kortings bon! 20% korting
+
+            //eerst even uitrekenen wat we dan eigenlijk betalen , 100% - 20%
+
+            double prijsProcentBetalen = 100 - 20;// maak hier de som  100% - 20% (% mag je weglaten)
+
+
+            //nu de echte prijs dan moeten we eerst de prijs door 100.0 delen en dan keer prijsProcentBetalen
+            //(niet)
+            double doorHonderd = totaalPrijs / 100;//maak hier de totalPrijs GEDEELD DOOR 100.0 som
+
+            double teBetalen = doorHonderd * prijsProcentBetalen;//nu doorHondered KEER prijsProcentBetalen
+
+            Console.WriteLine(prijsProcentBetalen);
+
+            //er moet nu 607.96 staan
+            Console.WriteLine($"te betalen {teBetalen}");
+
+            double teBetalenMakkelijk = totaalPrijs * 0.8; //makkelijkere manier
+
+            Console.WriteLine($"te betalen {teBetalenMakkelijk}"); //hetzelfde antwoord
+        }
+    }
+}
