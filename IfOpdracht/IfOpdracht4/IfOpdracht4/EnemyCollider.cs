fdf3ba330eb4e685c5659b1727babd5bc4aa65d2
Wessel <34840@ma-web.nl>
2023-02-21 14:44:45 +0100

    IfOpdracht

---
+++ IfOpdracht/IfOpdracht4/IfOpdracht4/EnemyCollider.cs
@@ -0,0 +1,35 @@
+﻿namespace IfOpdracht4
+{
+    internal class EnemyCollider
+    {
+        internal bool CanPassWall(Enemy enemy)
+        {
+            //gebruik hier een || (OR)
+            //je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+            //vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian) true is;
+            if (enemy.flying == true || enemy.incoporial == true || enemy.subterranian == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        internal bool CanPassWater(Enemy enemy)
+        {
+            //gebruik hier een || (OR)
+            //je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+            //vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian,swimmer) true is;
+            if (enemy.flying == true || enemy.incoporial == true || enemy.subterranian == true || enemy.swimmer == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
