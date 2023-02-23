2df1ff6eef1421ae1bad6e59ef93bfcfdd040b52
Wessel <34840@ma-web.nl>
2023-02-23 10:39:40 +0100

    shop greeter opdracht

---
+++ ShopGreeterOpdracht/ShopGreeterOpdracht/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassOefening;
+
+namespace ShopGreeterOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            ShopGreeter greeter = new ShopGreeter();
+
+            greeter.GreetCustomer();
+        }
+    }
+}
