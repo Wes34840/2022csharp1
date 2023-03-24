7f723e6bbcb36c8d8310012316ee4e3669c8d597
Wessel <34840@ma-web.nl>
2023-03-24 09:44:37 +0100

    Array Extra

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿    namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program Main = new Program();
+            int[] integers = new int[] { 10, 20, 30, 40, 50, 60 };
+            int Total = Main.GiveArray(integers);
+            Console.WriteLine(Total);
+
+        }
+        private int GiveArray(int[] Argument)
+        {
+            int Total = 0;
+            for (int i = 0; i < Argument.Length; i++)
+            {
+                Total += Argument[i];
+            }
+            return Total;
+        }
+    }
+}
